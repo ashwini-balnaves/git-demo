689fb55167237edb33cf702f85973456399623ce
Hauteclere <oliver.f.lavers@gmail.com>
2023-04-05 10:02:36 +1000

    added hello world

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
     path('projects/<int:pk>/', views.ProjectDetail.as_view()),
     path('pledges/', views.PledgeList.as_view()),
+    path('', views.HelloWorld.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -63,3 +63,7 @@ class PledgeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(supporter=self.request.user)
+
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({"hello": "world"})
